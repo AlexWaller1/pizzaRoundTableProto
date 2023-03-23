23413d0a6e945d9d9f9e2c32e51c38275dd2b71f
Alex Waller <alex@artofalexwaller.com>
2023-03-23 16:00:46 -0400

    BusinessPartnerRow

--- client3/src/components/BusinessPartnerRow.jsx
+++ client3/src/components/BusinessPartnerRow.jsx
@@ -3,9 +3,14 @@ import { useMutation } from '@apollo/client';
 import { DELETE_BUSINESS_PARTNER } from '../mutations/businessPartnerMutations';
 import { GET_BUSINESS_PARTNERS } from '../queries/businessPartnerQueries';
 import { GET_PIZZAS } from '../queries/pizzaQueries';
-
+// won't need this component either but this is what is displaying
+// business partner information on the home page, the businessPartner prop
+// that is passed down has a value of an element of the businessPartner
+// array that is on the data object that is being retrieved from the GraphQL query
 export default function BusinessPartnerRow({ businessPartner }) {
     const [deleteBusinessPartner] = useMutation(DELETE_BUSINESS_PARTNER, {
+        // here we are using a delete mutation, which means that an id variable
+        // needs to be passed through
         variables: { id: businessPartner.id },
         refetchQueries: [{ query: GET_BUSINESS_PARTNERS }, { query: GET_PIZZAS }],
         // update(cache, { data: { deleteBusinessPartner } }) {
