19aead405480109d3b94fb6c80af62781fd8a18d
Alex Waller <alex@artofalexwaller.com>
2023-03-23 19:30:31 -0400

    bootstrapClasses

--- client3/src/components/BusinessPartners.jsx
+++ client3/src/components/BusinessPartners.jsx
@@ -3,6 +3,8 @@ import BusinessPartnerRow from './BusinessPartnerRow';
 import { GET_BUSINESS_PARTNERS } from "../queries/businessPartnerQueries";
 import Spinner from "./Spinner";
 
+// Won't need this component
+// 
 export default function BusinessPartners() {
     const { loading, error, data } = useQuery(GET_BUSINESS_PARTNERS);
 
@@ -10,6 +12,7 @@ export default function BusinessPartners() {
     if (error) return <p>{"Dropped the Pizza :("}</p>
     return <>{ !loading && !error && (
         <table className="table table-hover mt-3">
+            {/* remember to look up bootstrap table classes */}
             <thead>
                 <tr>
                     <th>Name</th>
--- client3/src/components/DeletePizzaBtn.jsx
+++ client3/src/components/DeletePizzaBtn.jsx
@@ -6,14 +6,20 @@ import { useMutation } from "@apollo/client";
 
 export default function DeletePizzaBtn({ pizzaId }) {
     const navigate = useNavigate();
+    // function will help send user to home page when Pizza is deleted on its show page
 
     const [deletePizza] = useMutation(DELETE_PIZZA, {
       variables: { id: pizzaId },
+      // id needed to pick correct Pizza to delete
       onCompleted: () => navigate("/"),
+      // when mutation is made, user will be redirected to home page
       refetchQueries: [{ query: GET_PIZZAS }],
+      // we'll call GET_PIZZAS so list of current pizzas will be updated
+      // without users having to refresh page
     })
   return (
     <div className="d-flex mt-5 ms-auto">
+        {/* d is display, mt is margin-top, ms is margin-left, me is margin-right */}
         <button className="btn btn-danger m-2" onClick={deletePizza}>
             <FaTrash className="icon" /> Delete Pizza
         </button>
