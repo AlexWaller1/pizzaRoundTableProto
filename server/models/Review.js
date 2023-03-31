6063a2cba135dfce5a79ca473eae6a8c4c3e72c6
Alex Waller <alex@artofalexwaller.com>
2023-03-30 23:30:17 -0400

    ReviewSchema

---
+++ server/models/Review.js
@@ -0,0 +1,17 @@
+// Schema for Review Model for MongoDB Server
+const mongoose = require("mongoose");
+
+const ReviewSchema = new mongoose.Schema({
+  title: {
+    type: String
+  },
+  stars: {
+    type: String,
+    enum: ["1", "2", "3", "4", "5"]
+  },
+  text: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model("Review", ReviewSchema);
