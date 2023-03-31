ba6bbedc177fdd58a7ebc694fb9f038e63e3587c
Alex Waller <alex@artofalexwaller.com>
2023-03-30 23:31:28 -0400

    ReviewModel

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,7 @@ const BusinessPartner = require("../models/BusinessPartner");
 const Appetizer = require("../models/Appetizer");
 const Beverage = require("../models/Beverage");
 const Dessert = require("../models/Dessert");
+const Review = require("../models/Review");
 // models are imported here so they can be called for queries and mutations
 
 // GraphQL data types that will be used to define data types for properties
