5716d31eac957c47c06348809eababb07b71fdd7
Alex Waller <alex@artofalexwaller.com>
2023-04-04 00:53:44 -0400

    ADD_REVIEW

--- client3/src/mutations/ReviewMutations.js
+++ client3/src/mutations/ReviewMutations.js
@@ -1,1 +1,13 @@
 import { gql } from "@apollo/client";
+
+const ADD_REVIEW = gql`
+  mutation AddReview($title: String!, $stars: StarStatus!, $text: String!) {
+    addReview(title: $title, stars: $stars, text: $text) {
+      title
+      stars
+      text
+    }
+  }
+`;
+
+export { ADD_REVIEW };
