7459a142628689316e93e9d724e8bb83c9b03813
Alex Waller <alex@artofalexwaller.com>
2023-04-05 00:18:04 -0400

    DessertCard

--- client3/src/components/DessertCard.jsx
+++ client3/src/components/DessertCard.jsx
@@ -1,6 +1,18 @@
+import Beverage from "../../../server/models/Beverage";
 
-export default function DessertCard() {
+export default function DessertCard({ dessert }) {
   return (
-    <div>DessertCard</div>
+    <div className="col-md-4">
+        <div className="card mb-3">
+            <div className="card-body">
+                <div className="d-flex justify-content-between align-content-center">
+                    <h5 className="dessert-title">{dessert.title}</h5>
+                    <a className="btn btn-light" href={`/desserts/${dessert.id}`}></a>
+                </div>
+                <img src={dessert.image} alt="dessert image" />
+                <h6 className="dessert-price">{dessert.price}</h6>
+            </div>
+        </div>
+    </div>
   )
 }
