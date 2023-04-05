9e38f7e3bc150fbe79a2fd55ae1b326e1d9f02af
Alex Waller <alex@artofalexwaller.com>
2023-04-05 00:37:11 -0400

    RouterLinks()

--- client3/src/components/RouterLinks.jsx
+++ client3/src/components/RouterLinks.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { BrowswerRouter as Router, Routes, Routes, Link } from "react-router-dom";
+import Pizzas from './Pizzas';
+import Appetizers from './Appetizers';
+import Beverages from './Beverages';
+import Desserts from './Desserts';
 
 export default function RouterLinks() {
   return (
