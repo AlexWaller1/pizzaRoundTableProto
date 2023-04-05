b33dadfcff292df1792ceb58ec30e0199b72b705
Alex Waller <alex@artofalexwaller.com>
2023-04-04 23:59:06 -0400

    DessertsComponent

---
+++ client3/src/components/Desserts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useQuery } from '@apollo/client'
+import { GET_DESSERTS } from '../queries/dessertQueries';
+
+export default function Desserts() {
+  return (
+    <div>Desserts</div>
+  )
+}
