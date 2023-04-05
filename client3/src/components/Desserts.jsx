03753ad15ac4017e04539a3f6afef389347aca59
Alex Waller <alex@artofalexwaller.com>
2023-04-05 00:05:45 -0400

    Desserts()

--- client3/src/components/Desserts.jsx
+++ client3/src/components/Desserts.jsx
@@ -3,7 +3,16 @@ import { useQuery } from '@apollo/client'
 import { GET_DESSERTS } from '../queries/dessertQueries';
 
 export default function Desserts() {
+  const { loading, error, data } = useQuery(GET_DESSERTS);
+  if (loading) return <h3>Loading Desserts...</h3>
+  if (error) return <h3>Something Went Wrong</h3>
   return (
-    <div>Desserts</div>
+    data.desserts.length > 0 ? <div className="row mt-3">
+        {
+            data.desserts.map(dessert => (
+                <DessertCard key={dessert.id} dessert={dessert}/>
+            ))
+        }
+    </div> : <div>No Desserts</div>
   )
 }
