3138212202f021c60333fba37776d86664fbe622
Alex Waller <alex@artofalexwaller.com>
2023-04-04 23:08:53 -0400

    UPDATE_REVIEW DELETE_REVIEW

--- client3/src/mutations/ReviewMutations.js
+++ client3/src/mutations/ReviewMutations.js
@@ -10,4 +10,33 @@ const ADD_REVIEW = gql`
   }
 `;
 
+const UPDATE_REVIEW = gql`
+  mutation UpdateReview(
+    $id: ID!
+    $title: String!
+    $stars: StarStatusUpdate!
+    $text: String!
+  ) {
+    updateReview(id: $id, title: $title, stars: $stars, text: $text) {
+      id
+      title
+      stars
+      text
+    }
+  }
+`;
+
+const DELETE_REVIEW = gql`
+  mutation DeleteReview($id: ID!) {
+    deleteReview(id: $id) {
+      id
+      title
+      stars
+      text
+    }
+  }
+`;
+
 export { ADD_REVIEW };
+export { UPDATE_REVIEW };
+export { DELETE_REVIEW };
