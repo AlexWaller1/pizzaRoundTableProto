3a3ca87d5894fbda5b1d038230a48faf1182878a
Alex Waller <alex@artofalexwaller.com>
2023-04-04 23:41:17 -0400

    useQuery(GET_BEVERAGES)

--- client3/src/components/Beverages.jsx
+++ client3/src/components/Beverages.jsx
@@ -3,7 +3,7 @@ import { useQuery } from '@apollo/client';
 import { GET_BEVERAGES } from '../queries/beverageQueries';
 
 export default function Beverages() {
-  const { data, loading, error } = useQuery(GET_BEVERAGES);
+  const { loading, error, data } = useQuery(GET_BEVERAGES);
   if (loading) return <h3>Loading Beverages</h3>
   if (error) return <h3>Something Went Wrong</h3>
   return (
