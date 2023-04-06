7d8ca146a18e856e513a4337470f29c2d8d273a3
Alex Waller <alex@artofalexwaller.com>
2023-04-05 23:32:20 -0400

    DessertShowPage Imports

--- client3/src/components/DessertShowPage.jsx
+++ client3/src/components/DessertShowPage.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { useParams, Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_DESSERT } from '../queries/dessertQueries';
 
 export default function DessertShowPage() {
   return (
