ffa4c7970f667e72269faaf3e04d70a70a1f5435
Alex Waller <alex@artofalexwaller.com>
2023-04-05 23:14:58 -0400

    BeverageShowPage Imports

--- client3/src/components/BeverageShowPage.jsx
+++ client3/src/components/BeverageShowPage.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { useParams, Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_BEVERAGE } from '../queries/beverageQueries';
 
 export default function BeverageShowPage() {
   return (
