57b8817d984537b35cd2288f60c4afed5057f990
Alex Waller <alex@artofalexwaller.com>
2023-04-05 22:43:22 -0400

    PizzaShowPage()

--- client3/src/components/PizzaShowPage.jsx
+++ client3/src/components/PizzaShowPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
+import { GET_PIZZA } from '../queries/pizzaQueries';
 
 export default function PizzaShowPage() {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_PIZZA, {
+    variables: { id }
+  });
+
+  if (loading) return <h3>Loading Pizza...</h3>
+  if (error) return <h3>Something Went Wrong</h3>
+
   return (
-    <></>
+    <>
+      
+    </>
   )
 }
