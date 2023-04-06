13759a849b69fe7f2e99d96893ab097c93e77bb6
Alex Waller <alex@artofalexwaller.com>
2023-04-05 22:16:57 -0400

    NavLinks

--- client3/src/components/NavLinks.jsx
+++ client3/src/components/NavLinks.jsx
@@ -3,6 +3,22 @@ import { Link } from "react-router-dom";
 
 export default function NavLinks() {
   return (
-    <div>NavLinks</div>
+    <ul className="nav-links-list">
+        <h4>
+            <Link to="/">Home</Link>
+        </h4>
+        <h4>
+            <Link to="/pizzas">Pizzas</Link>
+        </h4>
+        <h4>
+            <Link to="/appetizers">Appetizers</Link>
+        </h4>
+        <h4>
+            <Link to="/beverages">Beverages</Link>
+        </h4>
+        <h4>
+            <Link to="/desserts">Desserts</Link>
+        </h4>
+    </ul>
   )
 }
