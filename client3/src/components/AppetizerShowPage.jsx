f20a0828ef81eb666660b97e27180690f4f0fd24
Alex Waller <alex@artofalexwaller.com>
2023-04-05 23:04:29 -0400

    Appetizer Show Page Query Setup

--- client3/src/components/AppetizerShowPage.jsx
+++ client3/src/components/AppetizerShowPage.jsx
@@ -4,7 +4,15 @@ import { useQuery } from "@apollo/client";
 import { GET_APPETIZER } from '../queries/appetizerQueries';
 
 export default function AppetizerShowPage() {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_APPETIZER, {
+    variables: { id }
+  })
+
+  if (loading) return <h3>Appetizer Loading</h3>
+  if (error) return <h3>Something Went Wrong</h3>
+
   return (
-    <div>AppetizerShowPage</div>
+    <></>
   )
 }
