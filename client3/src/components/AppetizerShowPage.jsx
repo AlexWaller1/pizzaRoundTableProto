fa8dcc942a785a2e33456150eb4533ba6e1063e5
Alex Waller <alex@artofalexwaller.com>
2023-04-05 23:11:27 -0400

    AppetizerShowPage()

--- client3/src/components/AppetizerShowPage.jsx
+++ client3/src/components/AppetizerShowPage.jsx
@@ -13,6 +13,26 @@ export default function AppetizerShowPage() {
   if (error) return <h3>Something Went Wrong</h3>
 
   return (
-    <></>
+    <>
+      {
+        !loading && !error && (
+            <div className="mx-auto w-75 card p-5">
+                <Link to="/appetizers" className='btn btn-light btn-sm w-25 d-inline ms-auto'>
+                    Back
+                </Link>
+                <div>
+                    <div>
+                        <img src={ data.appetizer.image } alt="appetizer image" />
+                    </div>
+                    <div>
+                        <h1>{ data.appetizer.name }</h1>
+                        <h3>{ data.appetizer.description }</h3>
+                        <h2>{ data.appetizer.price }</h2>
+                    </div>
+                </div>
+            </div>
+        )
+      }
+    </>
   )
 }
--- client3/src/components/RouterLinks.jsx
+++ client3/src/components/RouterLinks.jsx
@@ -5,6 +5,7 @@ import Appetizers from './Appetizers';
 import Beverages from './Beverages';
 import Desserts from './Desserts';
 import PizzaShowPage from './PizzaShowPage';
+import AppetizerShowPage from './AppetizerShowPage';
 
 export default function RouterLinks() {
   return (
@@ -12,6 +13,7 @@ export default function RouterLinks() {
         <Route path="/pizzas" element={<Pizzas/>} />
         <Route path="/pizzas/:id" element={<PizzaShowPage/>}/>
         <Route path="/appetizers" element={<Appetizers/>}/>
+        <Route path="/appetizers/:id" element={<AppetizerShowPage/>}/>
         <Route path="/beverages" element={<Beverages/>} />
         <Route path="/desserts" element={<Desserts/>}/>
     </Routes>
