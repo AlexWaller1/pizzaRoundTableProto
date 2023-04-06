af897437dbb7c4108b24b87ea0cffd0447f6469a
Alex Waller <alex@artofalexwaller.com>
2023-04-05 23:37:47 -0400

    DessertShowPageQuerySetup

--- client3/src/components/DessertShowPage.jsx
+++ client3/src/components/DessertShowPage.jsx
@@ -4,7 +4,15 @@ import { useQuery } from "@apollo/client";
 import { GET_DESSERT } from '../queries/dessertQueries';
 
 export default function DessertShowPage() {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_DESSERT, {
+    variables: { id }
+  });
+
+  if (loading) return <h3>Dessert Loading...</h3>
+  if (error) return <h3>Something Went Wrong</h3>
+
   return (
-    <div>DessertShowPage</div>
+    <></>
   )
 }
