66dbee25d72465f5040733c18c6bd72323a9b57e
Alex Waller <alex@artofalexwaller.com>
2023-04-05 23:45:37 -0400

    DessertShowPageSetup

--- client3/src/components/DessertShowPage.jsx
+++ client3/src/components/DessertShowPage.jsx
@@ -13,6 +13,26 @@ export default function DessertShowPage() {
   if (error) return <h3>Something Went Wrong</h3>
 
   return (
-    <></>
+    <>
+      {
+        !loading && !error && (
+            <div className="mx-auto w-75 card p-5">
+                <Link to="/desserts" className="btn btn-light btn-sm w-25 d-inline ms-auto">
+                    Back
+                </Link>
+                <div>
+                    <div>
+                        <img src={data.dessert.image} alt="dessert image" />
+                    </div>
+                    <div>
+                        <h1>{ data.dessert.name }</h1>
+                        <h3>{ data.dessert.description }</h3>
+                        <h2>{ data.dessert.price }</h2>
+                    </div>
+                </div>
+            </div>
+        )
+      }
+    </>
   )
 }
