91d6d824b870868cf32707c9e887ab733a5cdb58
Alex Waller <alex@artofalexwaller.com>
2023-04-18 22:24:51 -0400

    RouterLinksAndNavLinks

---
+++ client3/src/components/RouterLinksAndNavLinks.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RouterLinks from './RouterLinks'
+import NavLinks from './NavLinks'
+
+
+export default function RouterLinksAndNavLinks() {
+  return (
+    <>
+      <RouterLinks />
+      <NavLinks />
+    </>
+  )
+}
