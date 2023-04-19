900c2f21a6d2384396c7c8aaa400a0fee8496a4c
Alex Waller <alex@artofalexwaller.com>
2023-04-18 22:01:22 -0400

    addToCart

--- client3/src/components/RouterLinks.jsx
+++ client3/src/components/RouterLinks.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useId } from 'react'
 import { BrowswerRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Pizzas from './Pizzas';
 import Appetizers from './Appetizers';
@@ -10,16 +10,27 @@ import BeverageShowPage from './BeverageShowPage';
 import DessertShowPage from './DessertShowPage';
 
 export default function RouterLinks() {
+
+  const [cart, setCart] = useState([]);
+
+  let addToCart = (item) => {
+    let cartId = { cartId: useId() };
+
+    let cartItem = { ...cartId, ...item };
+
+    let newCart = [...cart, ...cartItem];
+  }
+
   return (
     <Routes>
         <Route path="/pizzas" element={<Pizzas/>} />
-        <Route path="/pizzas/:id" element={<PizzaShowPage/>}/>
+        <Route path="/pizzas/:id" element={<PizzaShowPage cart={ cart }/>}/>
         <Route path="/appetizers" element={<Appetizers/>}/>
-        <Route path="/appetizers/:id" element={<AppetizerShowPage/>}/>
+        <Route path="/appetizers/:id" element={<AppetizerShowPage cart={ cart }/>}/>
         <Route path="/beverages" element={<Beverages/>} />
-        <Route path="/beverages/:id" element={<BeverageShowPage/>}/>
+        <Route path="/beverages/:id" element={<BeverageShowPage cart={ cart }/>}/>
         <Route path="/desserts" element={<Desserts/>}/>
-        <Route path="/desserts/:id" element={<DessertShowPage/>} />
+        <Route path="/desserts/:id" element={<DessertShowPage cart={ cart }/>} />
     </Routes>
   )
 }
