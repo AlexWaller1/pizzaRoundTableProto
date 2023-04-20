10e7c84e765eab142e425f5c2e1d2ad71e3cb9e3
Alex Waller <alex@artofalexwaller.com>
2023-04-19 23:27:51 -0400

    CushetunkSpecial

--- client3/src/components/PizzaShowPage.jsx
+++ client3/src/components/PizzaShowPage.jsx
@@ -3,7 +3,7 @@ import { useParams, Link } from "react-router-dom";
 import { useQuery } from '@apollo/client';
 import { GET_PIZZA } from '../queries/pizzaQueries';
 
-export default function PizzaShowPage() {
+export default function PizzaShowPage({ addCartItem }) {
   const { id } = useParams();
   const { loading, error, data } = useQuery(GET_PIZZA, {
     variables: { id }
@@ -17,6 +17,7 @@ export default function PizzaShowPage() {
       {
         !loading && !error && (
             <div className='mx-auto w-75 card p-5'>
+                <button className="btn btn-primary btn-small w-25 d-inline ms-auto" onClick={() => addCartItem(data.pizza)}>Add To Cart</button>
                 <Link to="/pizzas" className="btn btn-light btn-sm w-25 d-inline ms-auto">
                     Back
                 </Link>
