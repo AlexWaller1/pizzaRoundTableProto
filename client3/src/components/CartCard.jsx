a59271801997cb7c2f3272b9871c9589aa31470f
Alex Waller <alex@artofalexwaller.com>
2023-04-20 22:35:30 -0400

    CartPrice

--- client3/src/components/CartCard.jsx
+++ client3/src/components/CartCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function CartCard({ cart }) {
   return (
-    <div className='col-md-4'>
-        <div className="card mb-3">
+    <div className='col-md-12'>
+        <div className="card mb-3 w-50">
             <div className="d-flex justify-content-between align-content-center">
                 <h5 className="cart-title">{cart.name}</h5>
             </div>
