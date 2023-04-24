100f792851385b8184b6adb4f3c888b9752212b3
Alex Waller <alex@artofalexwaller.com>
2023-04-24 00:41:47 -0400

    review card style changes

--- client3/src/components/ReviewCard.jsx
+++ client3/src/components/ReviewCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function ReviewCard({ review }) {
   return (
-    <div className="col-md-4">
+    <div className="col-md-12">
         <div className="card mb-3">
             <div className="card-body">
-                <div className="d-flex justify-content-between align-items-center">
+                <div className="d-block justify-content-between align-items-center">
                     <h3 className="review-title">{ review.title }</h3>
                     <img src={review.stars} alt="review stars" />
                     <h6 className="review-text">{ review.text }</h6>
