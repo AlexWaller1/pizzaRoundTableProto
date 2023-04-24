77e63790b891001afba36260526c1c1d4768d657
Alex Waller <alex@artofalexwaller.com>
2023-04-24 00:29:00 -0400

    ReviewCard

--- client3/src/components/ReviewCard.jsx
+++ client3/src/components/ReviewCard.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-export default function ReviewCard() {
+export default function ReviewCard({ review }) {
   return (
-    <div>ReviewCard</div>
+    <div className="col-md-4">
+        <div className="card mb-3">
+            <div className="card-body">
+                <div className="d-flex justify-content-between align-items-center">
+                    <h3 className="review-title">{ review.title }</h3>
+                    <img src={review.stars} alt="review stars" />
+                    <h6 className="review-text">{ review.text }</h6>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
