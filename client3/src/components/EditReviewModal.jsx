7f4b5d319f2f8394b77a13469b2887cacbf2131a
Alex Waller <alex@artofalexwaller.com>
2023-04-24 00:59:53 -0400

    onSubmit for EditReviewModal Form

--- client3/src/components/EditReviewModal.jsx
+++ client3/src/components/EditReviewModal.jsx
@@ -8,6 +8,20 @@ export default function EditReviewModal({ review }) {
   const [title, setTitle] = useState(review.title);
   const [stars, setStars] = useState(review.stars);
   const [text, setText] = useState(review.text);
+
+  const [updateReview] = useMutation(UPDATE_REVIEW, {
+    variables: { id: review.id, title, stars, text },
+    refetchQueries: [{ query: GET_REVIEW, variables: { id: review.id } }],
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!title || !stars || stars === "leave a star rating" || !text) {
+        return alert("Please Fill Out All Fields");
+    }
+
+    updateReview(title, stars, text);
+  }
   return (
     <div>EditReviewModal</div>
   )
