527b9cc4ad793f393e4401c8ff8127e5e8013f77
Alex Waller <alex@artofalexwaller.com>
2023-04-23 22:01:19 -0400

    AddReviewModal Setup

---
+++ client3/src/components/AddReviewModal.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_REVIEWS } from '../queries/reviewQueries';
+import { ADD_REVIEW } from '../mutations/reviewMutations';
+
+export default function AddReviewModal() {
+  return (
+    <div>AddReviewModal</div>
+  )
+}
--- client3/src/components/Reviews.jsx
+++ client3/src/components/Reviews.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+// import { GET_REVIEWS } from '../queries/reviewQueries';
 
 export default function Reviews() {
   return (
