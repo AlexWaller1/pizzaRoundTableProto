da67f72cf3ee9ecedaecb52f485cd7a2dec3dffa
Alex Waller <alex@artofalexwaller.com>
2023-04-24 00:32:39 -0400

    ReviewPage

--- client3/src/components/Reviews.jsx
+++ client3/src/components/Reviews.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { useQuery } from "@apollo/client";
 import { GET_REVIEWS } from '../queries/reviewQueries';
+import ReviewCard from './ReviewCard';
 
 export default function Reviews() {
   const { loading, error, data } = useQuery(GET_REVIEWS);
   if(loading) return <h3>Loading Reviews</h3>
   if(error) return <h3>Something Went Wrong</h3>
   return (
-    <></>
+    <>{
+        data.reviews.length > 0 ? <div className="row mt-3">
+            {
+                data.reviews.map(review => (
+                    <ReviewCard key={ review.id } review={ review } />
+                ))
+            }
+        </div> : <div></div>
+    }</>
   )
 }
