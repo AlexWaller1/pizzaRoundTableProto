a922847800db2a3cdf47e833c3d280b2bf2ea0bb
Alex Waller <alex@artofalexwaller.com>
2023-04-24 00:21:54 -0400

    ReviewCard

--- client3/src/components/Reviews.jsx
+++ client3/src/components/Reviews.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-// import { GET_REVIEWS } from '../queries/reviewQueries';
+import { useQuery } from "@apollo/client";
+import { GET_REVIEWS } from '../queries/reviewQueries';
 
 export default function Reviews() {
+  const { loading, error, data } = useQuery(GET_REVIEWS);
+  if(loading) return <h3>Loading Reviews</h3>
+  if(error) return <h3>Something Went Wrong</h3>
   return (
-    <div>Reviews</div>
+    <></>
   )
 }
