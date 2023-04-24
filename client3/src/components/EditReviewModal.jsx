2af9ad6a7ba662e2769ab08c1cb5be74632f2fd1
Alex Waller <alex@artofalexwaller.com>
2023-04-24 00:51:42 -0400

    EditReview Modal State Properties Setup

--- client3/src/components/EditReviewModal.jsx
+++ client3/src/components/EditReviewModal.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { GET_REVIEW } from '../queries/reviewQueries';
+import { UPDATE_REVIEW } from '../mutations/reviewMutations';
 
-export default function EditReviewModal() {
+export default function EditReviewModal({ review }) {
+  
+  const [title, setTitle] = useState(review.title);
+  const [stars, setStars] = useState(review.stars);
+  const [text, setText] = useState(review.text);
   return (
     <div>EditReviewModal</div>
   )
