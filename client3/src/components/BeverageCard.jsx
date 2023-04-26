517ff04e8bfa03c324c4c288923f5e4e211c797f
Alex Waller <alex@artofalexwaller.com>
2023-04-26 19:33:39 -0400

    adding jsx ids

--- client3/src/components/BeverageCard.jsx
+++ client3/src/components/BeverageCard.jsx
@@ -3,12 +3,14 @@ export default function BeverageCard({ beverage }) {
   return (
     <div className="col-md-6">
         <div className="card mb-3">
-            <div className="card-body">
-                <div className="d-flex justify-content-between align-items-center">
+            <div className="card-body" id="beverage-card">
+                <div className="d-flex justify-content-between align-items-center" id="beverage-title-and-btn-div">
                     <h5 className="beverage-title">{beverage.name}</h5>
-                    <a className="btn btn-light" href={`/beverages/${beverage.id}`}>View</a>
+                    <a className="btn btn-dark" href={`/beverages/${beverage.id}`} id="beverage-card-btn">View</a>
                 </div>
-                <img src={beverage.img} alt="beverage image" />
+                <div id="beverage-card-image-div">
+                  <img src={beverage.img} alt="beverage image" id="beverage-card-image"/>
+                </div>
                 <h6 className="beverage-price">{beverage.price}</h6>
             </div>
         </div>
