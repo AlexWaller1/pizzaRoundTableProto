c75e038da10242b11ab1e547376fa5b0b6a8b495
Alex Waller <alex@artofalexwaller.com>
2023-04-27 00:24:57 -0400

    Cart Stylings

--- client3/src/components/CartCard.jsx
+++ client3/src/components/CartCard.jsx
@@ -7,13 +7,17 @@ export default function CartCard({ cart }) {
     <div className='col-md-12' id="cart-card">
         <div className="card mb-3 w-70" id="cart-card-div">
             <div className="d-flex justify-content-between align-content-center" id="cart-card-name-and-btn-div">
-                <h5 className="cart-title">{cart.name}</h5>
-                <DeleteFromCartBtn cartId={cart.id} />
+                <h3 className="cart-title">{cart.name}</h3>
+                
             </div>
             <div id="cart-card-image-div">
-              <img src={cart.image} alt="cart-image" id="cart-image"/>
+              <img src={cart.image} alt="cart-image" className="mt-5" id="cart-image"/>
             </div>
-            <h6 className="cart-price">{cart.price}</h6>
+            <div className="d-flex justify-content-between" id="cart-card-price-div">
+              <h4 className="mt-5" id="cart-price">{cart.price}</h4>
+              <DeleteFromCartBtn cartId={cart.id} />
+            </div>
+            
         </div>
     </div>
   )
