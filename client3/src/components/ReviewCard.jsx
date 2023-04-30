9a0571aa8a50c6a36f9c7cfbad9b184acc5b0fb4
Alex Waller <alex@artofalexwaller.com>
2023-04-30 18:55:55 -0400

    ReviewCardModifications

--- client3/src/components/ReviewCard.jsx
+++ client3/src/components/ReviewCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./ReviewCard.css";
+import ViewBtnIcon from './ViewBtnIcon';
 
 export default function ReviewCard({ review }) {
   return (
@@ -9,7 +10,12 @@ export default function ReviewCard({ review }) {
                 <div className="d-flex justify-content-between align-items-center" id="review-content">
                     <h3 className="review-title">{ review.title }</h3>
                     <img src={review.stars} alt="review stars" id="review-stars" />
-                    <a className="btn btn-dark" href={`/reviews/${review.id}`}><h5>View</h5></a>
+                    <a className="btn btn-dark" href={`/reviews/${review.id}`}>
+                        <div className="d-flex">
+                          <h5 className="view-review-btn-text">View</h5>
+                          <ViewBtnIcon />
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
