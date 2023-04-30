112d21ad0691d79dcd16148b9c1a624f16b3a01c
Alex Waller <alex@artofalexwaller.com>
2023-04-30 18:43:48 -0400

    ViewBtnIconChanges

--- client3/src/components/ViewBtnIcon.jsx
+++ client3/src/components/ViewBtnIcon.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 
 export default function ViewBtnIcon() {
   return (
-    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
-    width="40px" height="40px" viewBox="0 0 1728 1728" className="view-btn-icon" >
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" height="40px" width="40px" viewBox="0 0 1728 1728" className="view-btn-icon">
         <g id="Layer_3">
-            <path className="st2" fill="#fff" d="M1629.23,610.5C1589.55,565.73,1297.53,247.45,827,262C393.57,275.4,132.1,561.22,88.5,610.5
-                c13.31,21.38,283.03,437.87,785.5,429.5C1357.2,1031.95,1611.86,638.41,1629.23,610.5z"/>
-            <path className="st2" fill="#fff" d="M766.99,264.52C619.5,330.53,521.43,473.06,514.4,631.9c-7.46,168.74,89.08,327.38,244.51,401.1"/>
-            <path className="st2" fill="#fff" d="M1159.29,308.38c17.33,10.52,175.5,110.3,192.71,303.62c13.21,148.35-61.76,293.78-192.71,374.83"/>
+            <path class="st1" fill='#fff' d="M1684,546c-45.25-48.02-405.69-417.82-937.83-360C356.86,228.3,114.73,473.51,48,546
+                c11.81,20.25,292.92,483.24,828.84,477C1398.78,1016.92,1669.13,571.5,1684,546z"/>
+            <ellipse class="st1" fill="dark-gray" cx="863.67" cy="609" rx="489.99" ry="414"/>
             <g>
-                <ellipse className="st3" fill="dark-gray" cx="1061.5" cy="648" rx="202.5" ry="196"/>
+                <ellipse class="st2" fill="#fff" cx="870.5" cy="600.5" rx="265.72" ry="272.5"/>
             </g>
         </g>
     </svg>
