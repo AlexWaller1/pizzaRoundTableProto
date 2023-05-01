f45209439bb83af9d9f9c6da2074891d7e4930ac
Alex Waller <alex@artofalexwaller.com>
2023-05-01 19:46:21 -0400

    DeleteFromCartBtnIcon

---
+++ client3/src/components/DeleteFromCartBtnIcon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function DeleteFromCartBtnIcon() {
+  return (
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 1440 1080">
+            <g id="Layer_3">
+                <path class="st0" d="M1301,201.67v596H551.67C409.89,687.89,268.11,578.11,126.33,468.33c132-88.89,264-177.78,396-266.67H1301z"/>
+                <line class="st0" x1="1138.33" y1="311" x2="626.33" y2="692.33"/>
+                <line class="st0" x1="626.33" y1="311" x2="1175.67" y2="692.33"/>
+            </g>
+    </svg>
+
+  )
+}
