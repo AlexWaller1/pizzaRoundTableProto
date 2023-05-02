7c725a62cae2e7b6f9acc8b5554a73d2136a4e91
Alex Waller <alex@artofalexwaller.com>
2023-05-01 20:10:44 -0400

    DeleteFromCartBtnIcon

--- client3/src/components/DeleteFromCartBtn.jsx
+++ client3/src/components/DeleteFromCartBtn.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { DELETE_CART } from '../mutations/cartMutations';
 import { GET_CARTS } from '../queries/cartQueries';
 import { useMutation } from '@apollo/client';
+import DeleteFromCartBtnIcon from './DeleteFromCartBtnIcon';
 import "./DeleteFromCartBtn.css";
 
 export default function DeleteFromCartBtn({ cartId }) {
@@ -11,8 +12,11 @@ export default function DeleteFromCartBtn({ cartId }) {
   })
   return (
     <div className="d-flex mt-5 ms-auto" id="delete-from-cart-btn-div">
-        <button className="btn btn-dark m-2" id="delete-from-cart-btn" onClick={deleteFromCart}>
-           <h5 className="delete-from-cart-btn-text">Delete From Cart</h5>
+        <button className="d-flex btn btn-dark m-2" id="delete-from-cart-btn" onClick={deleteFromCart}>
+          <div className='mt-1'>
+            <DeleteFromCartBtnIcon />
+          </div>
+          <h5 className="delete-from-cart-btn-text">Delete From Cart</h5>
         </button>
     </div>
   )
--- client3/src/components/DeleteFromCartBtnIcon.jsx
+++ client3/src/components/DeleteFromCartBtnIcon.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 export default function DeleteFromCartBtnIcon() {
   return (
     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
-	 viewBox="0 0 1440 1080">
-            <g id="Layer_3">
-                <path class="st0" d="M1301,201.67v596H551.67C409.89,687.89,268.11,578.11,126.33,468.33c132-88.89,264-177.78,396-266.67H1301z"/>
-                <line class="st0" x1="1138.33" y1="311" x2="626.33" y2="692.33"/>
-                <line class="st0" x1="626.33" y1="311" x2="1175.67" y2="692.33"/>
-            </g>
+	 width="30px" heigth="30px" viewBox="0 0 72 36">
+
+        <g id="Layer_3">
+            <path class="st1" fill="#333333" d="M69.6,2.74v30.51H22.25c-7-5.55-14-11.1-21-16.65C7.82,11.99,14.4,7.36,20.97,2.74H69.6z"/>
+        </g>
+        <g id="Layer_4">
+            <polygon class="st2" fill="none" stroke="#F9F9FA" stroke-width={2} stroke-miterlimit={10} points="69.6,33.26 69.6,2.74 21.66,2.74 2.22,16.61 23.04,33.26 	"/>
+        </g>
+        <g id="Layer_5">
+            <line class="st2" fill="none" stroke="#F9F9FA" stroke-width={4} stroke-miterlimit={10} x1="58.54" y1="7.86" x2="30.95" y2="27.49"/>
+            <path class="st2" fill="none" stroke="#F9F9FA" stroke-width={4} stroke-miterlimit={10} d="M33.43,7.08c8.55,7.28,17.1,14.56,25.66,21.83"/>
+        </g>
     </svg>
 
   )
