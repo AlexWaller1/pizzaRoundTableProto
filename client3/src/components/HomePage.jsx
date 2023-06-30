2af21737dd96617c49eafcac56d65f365b615a77
Alex Waller <alex@artofalexwaller.com>
2023-06-30 00:19:39 -0400

    changingFonts

--- client3/src/components/HomePage.jsx
+++ client3/src/components/HomePage.jsx
@@ -9,19 +9,19 @@ export default function HomePage() {
     <>
       <div id="home-page-div">
         <div id="home-page-nested-div-1">
-          <h2 id="home-page-header-div">The Best Pizza in New Jersey!</h2>
-          <HomePageImage1 height="400px" width="800px"/>
+          <h1 id="home-page-header-1">The Best Pizza in New Jersey!</h1>
+          <HomePageImage1 height="800px" width="1600px"/>
         </div>
         <div id="home-page-nested-div-2">
-          <h3 id="home-page-header-div-2">Pizza, Appetizers, Desserts, and Beverages</h3>
-          <HomePageImage2 height="400px" width="800px"/>
+          <h1 id="home-page-header-div-2">All Dough and Sauce Made In House!</h1>
+          <HomePageImage2 height="800px" width="1600px"/>
         </div>
         <div id="home-page-nested-div-2">
           <h6 id="home-page-text">Pizza Roundtable offers Italian Pizza unique to Whitehouse Station of New Jersey.
               Offering excellent, homemade crust with the fineset cheeses, meats, and sauces, we
               offer the Italian classics and go to great lengths to ensure our customers get the 
               best and freshest ingredients.
-              <HomePageImage3 height="400px" width="800px" />
+              <HomePageImage3 height="800px" width="1600px" />
           </h6>
         </div>
        
